09200e9d57912957869d0df12db7078e42f553d9
NithinSai412 <83264954+NithinSai412@users.noreply.github.com>
2025-07-14 22:52:39 +0530

    Create helloWorld.pl

---
+++ Perl_Fundamentals/helloWorld.pl
@@ -0,0 +1,7 @@
+# -- First program in Perl -- 
+
+use strict; #This is called as pragma, this pragma tells perl to enforce rules onto our code
+use warnings; #This pragma tells perl to turn on additional warning reporting.
+print "Hello, World!\n";
+# Unlike TCL, perl does not automatically write to the new line after printing the text. Hence, we need to use "\n" at the end.
+print "How is it going?";
